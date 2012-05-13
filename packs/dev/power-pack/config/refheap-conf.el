d89f1ceab2946e1a93eeda79f70c5190c38a8fa8
Sam Aaron <samaaron@gmail.com>
2012-05-13 18:32:32 +0100

    update refheap config comment to use setq rather than custom-set-variables

--- packs/dev/power-pack/config/refheap-conf.el
+++ packs/dev/power-pack/config/refheap-conf.el
@@ -1,5 +1,6 @@
 ;;Create a file called ~/.refheap-pass with the following (use your own credentials):
-;;(custom-set-variables '(refheap-token "your token") '(refheap-user "your username"))
+;;(setq refheap-token "your token"
+;;      refheap-user "your username")
 
 (live-add-pack-lib "refheap")
 (if (file-exists-p "~/.refheap-pass")
