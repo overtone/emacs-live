5edbb2bf33d9565740803e9b49c6c5f98f5d2430
Sam Aaron <samaaron@gmail.com>
2012-06-07 17:08:10 +0100

    override pcache's default cache dir to sit inside live-tmp-dir

--- packs/dev/power-pack/config/gist-conf.el
+++ packs/dev/power-pack/config/gist-conf.el
@@ -1,3 +1,9 @@
+;;override pcache's default cache dir
+(defvar pcache-directory
+  (let ((dir (file-name-as-directory (concat live-tmp-dir "pcache"))))
+    (make-directory dir t)
+    dir))
+
 (live-add-pack-lib "gist")
 (live-add-pack-lib "gh")
 (live-add-pack-lib "pcache")
