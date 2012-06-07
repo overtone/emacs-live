f67d210a08c3b336a72b93ab95254b7e13497e8b
Sam Aaron <samaaron@gmail.com>
2012-06-07 14:22:23 +0100

    update refheap password file to ~/.refheap-pass.el

--- packs/dev/power-pack/config/refheap-conf.el
+++ packs/dev/power-pack/config/refheap-conf.el
@@ -1,9 +1,9 @@
-;;Create a file called ~/.refheap-pass with the following (use your own credentials):
+;;Create a file called ~/.refheap-pass.el with the following (use your own credentials):
 ;;(setq refheap-token "your token"
 ;;      refheap-user "your username")
 
 (live-add-pack-lib "refheap")
-(if (file-exists-p "~/.refheap-pass")
-    (load "~/.refheap-pass"))
+(if (file-exists-p "~/.refheap-pass.el")
+    (load "~/.refheap-pass.el"))
 
 (require 'refheap)
