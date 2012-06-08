0fe173920ce024c19c3940c1581cc152fd976ead
Sam Aaron <samaaron@gmail.com>
2012-06-08 23:48:34 +0100

    Awesome - display slime description buffers in an ephemeral popup.

--- packs/dev/foundation-pack/config/popwin-conf.el
+++ packs/dev/foundation-pack/config/popwin-conf.el
@@ -1,3 +1,9 @@
 (live-add-pack-lib "popwin")
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
+
+(setq popwin:special-display-config
+      '(("*Help*") ("*Completions*" :noselect t)
+        ("*compilation*" :noselect t)
+        ("*Occur*" :noselect t)
+        ("\\*Slime Description.*" :noselect t :regexp t)))
