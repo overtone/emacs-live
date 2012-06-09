a891c380b2d3e03756b3349cb64df1775a427c11
Sam Aaron <samaaron@gmail.com>
2012-06-10 00:38:33 +0100

    further popwin-conf tweaks

--- packs/dev/foundation-pack/config/popwin-conf.el
+++ packs/dev/foundation-pack/config/popwin-conf.el
@@ -3,7 +3,8 @@
 (setq display-buffer-function 'popwin:display-buffer)
 
 (setq popwin:special-display-config
-      '(("*Help*") ("*Completions*" :noselect t)
+      '(("*Help*"  :position right :width 80)
+        ("*Completions*" :noselect t)
         ("*compilation*" :noselect t)
         ("*Occur*" :noselect t)
         ("\\*Slime Description.*" :noselect t :regexp t :position right :width 74)
