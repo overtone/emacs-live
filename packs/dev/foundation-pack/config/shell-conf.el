622f705f3873a21256b1fdb7ab4f980c38fa0117
Sam Aaron <samaaron@gmail.com>
2012-06-12 16:40:32 +0100

    add more goodies to shell-conf

--- packs/dev/foundation-pack/config/shell-conf.el
+++ packs/dev/foundation-pack/config/shell-conf.el
@@ -9,3 +9,28 @@
 ;;make sure ansi colour character escapes are honoured
 (require 'ansi-color)
 (ansi-color-for-comint-mode-on)
+
+;; kill buffer when terminal process is killed
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+  (if (memq (process-status proc) '(signal exit))
+      (let ((buffer (process-buffer proc)))
+        ad-do-it
+        (kill-buffer buffer))
+    ad-do-it))
+(ad-activate 'term-sentinel)
+
+(defun live-term-use-utf8 ()
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+(add-hook 'term-exec-hook 'live-term-use-utf8)
+
+(defun live-term-paste (&optional string)
+ (interactive)
+ (process-send-string
+  (get-buffer-process (current-buffer))
+  (if string string (current-kill 0))))
+
+(defun live-term-hook ()
+  (goto-address-mode)
+  (define-key term-raw-map "\C-y" 'live-term-paste))
+
+(add-hook 'term-mode-hook 'live-term-hook)
