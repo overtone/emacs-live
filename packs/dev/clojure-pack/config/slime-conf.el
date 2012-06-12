7900965f16ba80228418c398ce3719ca20c805bc
Sam Aaron <samaaron@gmail.com>
2012-06-12 11:18:03 +0100

    add live-clojure-reset-namespace fn. Thanks to yuanmai for suggesting this

--- packs/dev/clojure-pack/config/slime-conf.el
+++ packs/dev/clojure-pack/config/slime-conf.el
@@ -17,3 +17,13 @@
                                   (modify-syntax-entry ?\} "){")
                                   (modify-syntax-entry ?\[ "(]")
                                   (modify-syntax-entry ?\] ")[")))
+
+(defun live-clojure-reset-namespace ()
+  "Reloads the current Clojure namespace by first removing it and
+then re-evaluating the slime buffer. Use this to remove old
+symbol definitions and reset the namespace to contain only what
+is defined in your current Emacs buffer."
+  (interactive)
+  (save-buffer)
+  (slime-interactive-eval (concat "(remove-ns '" (slime-current-package) ")"))
+  (slime-compile-and-load-file))
