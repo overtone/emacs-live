1da7d7affca01736ef25e24555042bc31ae3fcf2
Sam Aaron <samaaron@gmail.com>
2012-06-12 17:42:09 +0100

    add useful paredit navigation fns to paredit-conf

--- packs/dev/clojure-pack/config/paredit-conf.el
+++ packs/dev/clojure-pack/config/paredit-conf.el
@@ -3,3 +3,23 @@
   '(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp))
 
 (require 'paredit)
+
+(defun live-paredit-next-top-level-form ()
+  (interactive)
+  (while (ignore-errors (paredit-backward-up) t))
+  (paredit-forward)
+  (ignore-errors (search-forward "(")
+                 (backward-char)))
+
+(defun live-paredit-previous-top-level-form ()
+  (interactive)
+  (while (ignore-errors (paredit-backward-up) t))
+  (paredit-backward))
+
+(defun live-paredit-forward ()
+  "Feels more natural to move to the beginning of the next item
+in the sexp, not the end of the current one."
+  (interactive)
+  (paredit-forward)
+  (paredit-forward)
+  (paredit-backward))
