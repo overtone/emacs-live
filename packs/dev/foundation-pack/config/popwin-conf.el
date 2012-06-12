c2d86da568e368987a33afe1fb1727b8ace3b8d6
Sam Aaron <samaaron@gmail.com>
2012-06-12 16:40:56 +0100

    further poplin tweaks and a utility fn to show the *Messages* buffer

--- packs/dev/foundation-pack/config/popwin-conf.el
+++ packs/dev/foundation-pack/config/popwin-conf.el
@@ -3,15 +3,22 @@
 (setq display-buffer-function 'popwin:display-buffer)
 
 (setq popwin:special-display-config
-      '(("*Help*"  :position right :width 80)
+      '(("*Help*"  :height 30 :stick t)
         ("*Completions*" :noselect t)
         ("*compilation*" :noselect t)
+        ("*Messages*" :height 30)
         ("*Occur*" :noselect t)
-        ("\\*Slime Description.*" :noselect t :regexp t :position right :width 74)
+        ("\\*Slime Description.*" :noselect t :regexp t :height 30)
         ("*magit-commit*" :noselect t :height 40 :width 80)
         ("*magit-diff*" :noselect t :height 40 :width 80)
         ("*magit-edit-log*" :noselect t :height 15 :width 80)
         ("\\*Slime Inspector.*" :regexp t :height 30)
-        ("*Ido Completions*" :noselect t)
+        ("*Ido Completions*" :noselect t :height 30)
         ("*eshell*" :height 30)
+        ("*ansi-term*" :height 30)
         ("*shell*" :height 30)))
+
+
+(defun live-show-messages ()
+  (interactive)
+  (popwin:display-buffer "*Messages*"))
