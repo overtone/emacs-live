d3e6cee54d93aefabca9236bd333757061b3bbc4
Sam Aaron <samaaron@gmail.com>
2012-06-14 11:47:02 +0100

    Enable eldoc in elisp buffers

--- packs/dev/foundation-pack/config/lisp-conf.el
+++ packs/dev/foundation-pack/config/lisp-conf.el
@@ -1,3 +1,7 @@
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
 (define-key lisp-mode-shared-map (kbd "RET") 'reindent-then-newline-and-indent)
 
 (dolist (x '(scheme emacs-lisp lisp))
