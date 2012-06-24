534a5c98b379ac18a9c0bf525a585eaeefe1ed9c
Sam Aaron <samaaron@gmail.com>
2012-06-24 22:54:04 +0100

    Add live versions of forward and backward kill sexp fns.

--- packs/dev/clojure-pack/config/paredit-conf.el
+++ packs/dev/clojure-pack/config/paredit-conf.el
@@ -71,3 +71,15 @@ in the sexp, not the end of the current one."
              (end-of-defun)
              (beginning-of-defun)
              (indent-sexp)))))
+
+(defun live-paredit-forward-kill-sexp (&optional arg)
+  (interactive "p")
+  (cond ((or (paredit-in-comment-p)
+             (paredit-in-string-p)) (kill-word (or arg 1)))
+        (t (kill-sexp (or arg 1)))))
+
+(defun live-paredit-backward-kill-sexp (&optional arg)
+  (interactive "p")
+  (cond ((or (paredit-in-comment-p)
+             (paredit-in-string-p)) (backward-kill-word (or arg 1)))
+        (t (backward-kill-sexp (or arg 1)))))
