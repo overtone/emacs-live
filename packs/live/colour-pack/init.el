f33be6746c118ffdf9841440356d1a4ff9c2eab7
Sam Aaron <samaaron@gmail.com>
2012-07-10 11:23:48 +0100

    Update live packs to reflect dev work. See previous commits for history.

--- packs/live/colour-pack/init.el
+++ packs/live/colour-pack/init.el
@@ -7,6 +7,7 @@
 ;; use blackbored colour theme
 (load-file (concat (live-pack-lib-dir) "cyberpunk.el"))
 (load-file (concat (live-pack-lib-dir) "gandalf.el"))
+
 (color-theme-cyberpunk)
 
 (set-cursor-color "yellow")
