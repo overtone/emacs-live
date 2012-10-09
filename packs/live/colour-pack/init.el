3fdcebbbc72c2d5ebd5882f2476934d909683de0
Sam Aaron <samaaron@gmail.com>
2012-10-09 10:23:51 +0100

    update live packs to reflect dev work. See previous commit for history

--- packs/live/colour-pack/init.el
+++ packs/live/colour-pack/init.el
@@ -2,7 +2,6 @@
 (require 'color-theme)
 
 (global-hl-line-mode 1)
-(set-face-background 'hl-line "#333333")
 
 ;; use blackbored colour theme
 (load-file (concat (live-pack-lib-dir) "cyberpunk.el"))
