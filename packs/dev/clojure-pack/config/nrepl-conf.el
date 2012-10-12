c3858e728733faf23cbd7e9bb2670d14283b8fb3
Sam Aaron <samaaron@gmail.com>
2012-10-12 14:27:02 +0300

    temporarily disable auto-complete in nrepl mode until I fix it - it currently dies horribly.

--- packs/dev/clojure-pack/config/nrepl-conf.el
+++ packs/dev/clojure-pack/config/nrepl-conf.el
@@ -33,8 +33,10 @@
 (require 'ac-nrepl )
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
+
+
+;; (eval-after-load "auto-complete"
+;;   '(add-to-list 'ac-modes 'nrepl-mode))
 
 
 ;;; Monkey Patch nREPL with better behaviour:
