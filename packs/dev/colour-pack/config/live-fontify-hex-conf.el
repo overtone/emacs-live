e9ee0e4efeb0feef0c7c0ff525e30a2e818b91e4
Sam Aaron <samaaron@gmail.com>
2012-11-19 17:46:37 +0000

    add live-fontify-hex

---
+++ packs/dev/colour-pack/config/live-fontify-hex-conf.el
@@ -0,0 +1,11 @@
+(live-add-pack-lib "live-fontify-hex")
+(require 'live-fontify-hex)
+
+(font-lock-add-keywords 'lisp-mode
+                        '((live-fontify-hex-colors)))
+
+(font-lock-add-keywords 'emacs-lisp-mode
+                        '((live-fontify-hex-colors)))
+
+(font-lock-add-keywords 'lisp-interaction-mode
+                        '((live-fontify-hex-colors)))
