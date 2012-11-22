892281cddce20ce945ba8fc2ba7eb01e76c6f9f4
Sam Aaron <samaaron@gmail.com>
2012-11-22 13:20:10 +0000

    don't attempt to ido complete filename at point

--- packs/dev/foundation-pack/config/ido-conf.el
+++ packs/dev/foundation-pack/config/ido-conf.el
@@ -4,7 +4,6 @@
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
       ido-max-prospects 10
       ido-default-file-method 'selected-window)
 
