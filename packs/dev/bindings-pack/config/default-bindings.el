13974912f7b3a97f06cf0539e787a5c25f5d1f29
Sam Aaron <samaaron@gmail.com>
2012-11-25 10:16:53 +0000

    add binding for evaluating a buffer with nrepl

--- packs/dev/bindings-pack/config/default-bindings.el
+++ packs/dev/bindings-pack/config/default-bindings.el
@@ -37,6 +37,10 @@
 (global-set-key (kbd "C-c m n")     'eval-print-last-sexp)
 (global-set-key (kbd "C-c m r")     'eval-region)
 
+(global-set-key (kbd "C-c n e b")     'nrepl-eval-buffer)
+
+
+
 ;;funky characters
 (global-set-key (kbd "C-c i l") (lambda () (interactive) (insert "λ")))
 (global-set-key (kbd "C-c i n") (lambda () (interactive) (insert "ℕ")))
