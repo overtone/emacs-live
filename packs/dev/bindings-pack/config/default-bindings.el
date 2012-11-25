641bcd2a920a77f62ee42df40b3703ce6242dbd1
Sam Aaron <samaaron@gmail.com>
2012-11-25 10:17:48 +0000

    override C-w in ido-file-completion mode to backward-kill-word

--- packs/dev/bindings-pack/config/default-bindings.el
+++ packs/dev/bindings-pack/config/default-bindings.el
@@ -128,6 +128,7 @@
 ;;allow the deletion of words:
 ;;backward kill word (forward kill word is M-d)
 (global-set-key (kbd "C-w") 'backward-kill-word)
+(define-key ido-file-completion-map (kbd "C-w") 'backward-kill-word)
 (define-key paredit-mode-map (kbd "C-w") 'paredit-backward-kill-word)
 
 ;;kill line backwards
