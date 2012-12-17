8dd16f6625a109d876999f0661d24b0cf453657d
Sam Aaron <samaaron@gmail.com>
2012-12-17 17:11:03 +0000

    update to nrep.el kludge fix for popwin craziness.

--- packs/dev/foundation-pack/config/popwin-conf.el
+++ packs/dev/foundation-pack/config/popwin-conf.el
@@ -21,8 +21,7 @@
         (".*overtone.log" :regexp t :height 30)
         ("*gists*" :height 30)
         ("*sldb.*":regexp t :height 30)
-        ;; TODO: fix nREPL + popwin conflicts and reinstate.
-        ;;("*nrepl-error*" :height 30)
+        ("*nrepl-error*" :height 30)
         ("*nREPL doc*" :height 30)
         ("*Kill Ring*" :height 30)
         ("*Compile-Log*" :height 30)))
