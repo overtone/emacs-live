93a9f3518524b171230f8bfbed46bc73f914db2f
Sam Aaron <samaaron@gmail.com>
2013-02-23 10:05:40 +0000

    modify git-gutter settings

--- packs/dev/power-pack/config/git-gutter-conf.el
+++ packs/dev/power-pack/config/git-gutter-conf.el
@@ -1,8 +1,16 @@
 (live-add-pack-lib "emacs-git-gutter")
 (require 'git-gutter)
 
-
-(global-git-gutter-mode t)
+(global-git-gutter-mode nil)
+(setq git-gutter:window-width 2)
 
 (setq git-gutter:diff-option "-w")
 (setq git-gutter:lighter " GG")
+
+(setq git-gutter:modified-sign "~") ;; two space
+(setq git-gutter:added-sign "+")    ;; multiple character is OK
+(setq git-gutter:deleted-sign "-")
+
+(set-face-background 'git-gutter:modified "purple") ;; background color
+(set-face-foreground 'git-gutter:added "green")
+(set-face-foreground 'git-gutter:deleted "red")
