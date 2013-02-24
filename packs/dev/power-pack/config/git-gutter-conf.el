c6c59fcd296be1ac55774011cd50b444fd22103e
Sam Aaron <samaaron@gmail.com>
2013-02-24 19:13:41 +0000

    update git gutter and enable by default (always showing gutter)

--- packs/dev/power-pack/config/git-gutter-conf.el
+++ packs/dev/power-pack/config/git-gutter-conf.el
@@ -1,11 +1,14 @@
 (live-add-pack-lib "emacs-git-gutter")
 (require 'git-gutter)
 
-(global-git-gutter-mode nil)
 (setq git-gutter:window-width 2)
 
-(setq git-gutter:lighter "GG")
+(global-git-gutter-mode t)
 
-(setq git-gutter:modified-sign "~") ;; two space
-(setq git-gutter:added-sign "+")    ;; multiple character is OK
-(setq git-gutter:deleted-sign "-")
+(setq git-gutter:lighter " ++")
+(setq git-gutter:always-show-gutter t)
+
+(setq git-gutter:modified-sign "~ ") ;; two space
+(setq git-gutter:added-sign "+ ")    ;; multiple character is OK
+(setq git-gutter:deleted-sign "- ")
+(setq git-gutter:unchanged-sign "  ")
