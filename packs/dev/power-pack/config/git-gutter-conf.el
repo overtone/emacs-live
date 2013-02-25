c933b1ab0d23df0dea6e5cb731da2c35f769ab66
Sam Aaron <samaaron@gmail.com>
2013-02-25 07:55:50 +0000

    minor tweakes to git-gutter config

--- packs/dev/power-pack/config/git-gutter-conf.el
+++ packs/dev/power-pack/config/git-gutter-conf.el
@@ -5,10 +5,10 @@
 
 (global-git-gutter-mode t)
 
-(setq git-gutter:lighter " ++")
+(setq git-gutter:lighter " G-+")
 (setq git-gutter:always-show-gutter t)
 
-(setq git-gutter:modified-sign "~ ") ;; two space
-(setq git-gutter:added-sign "+ ")    ;; multiple character is OK
+(setq git-gutter:modified-sign "~ ")
+(setq git-gutter:added-sign "+ ")
 (setq git-gutter:deleted-sign "- ")
 (setq git-gutter:unchanged-sign "  ")
