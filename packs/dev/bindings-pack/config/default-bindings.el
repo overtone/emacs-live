5866abf184cb35896b2572ee0294192fbd6316ef
Sam Aaron <samaaron@gmail.com>
2013-03-06 14:45:42 +0000

    add live-recentf-ido-find-file

--- packs/dev/bindings-pack/config/default-bindings.el
+++ packs/dev/bindings-pack/config/default-bindings.el
@@ -185,7 +185,7 @@
 ;; File
 (global-set-key (kbd "C-x M-f")   'ido-find-file-other-window)
 (global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
-(global-set-key (kbd "C-x f")     'recentf-ido-find-file)
+(global-set-key (kbd "C-x f")     'live-recentf-ido-find-file)
 (global-set-key (kbd "C-x C-r")   'ido-recentf-open)
 (global-set-key (kbd "M-`")       'file-cache-minibuffer-complete)
 (global-set-key (kbd "C-x C-b")   'ibuffer)
