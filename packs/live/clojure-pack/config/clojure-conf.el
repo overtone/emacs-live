734f21b81c83c956cfa2c72cd3e499c95cb3d9a8
Jay Fields <github@jayfields.com>
2013-05-11 15:36:09 -0400

    add toggle-clj-keyword-string

--- packs/live/clojure-pack/config/clojure-conf.el
+++ packs/live/clojure-pack/config/clojure-conf.el
@@ -55,3 +55,35 @@
 (dolist (x '(scheme emacs-lisp lisp clojure))
   (add-hook (intern (concat (symbol-name x) "-mode-hook")) 'enable-paredit-mode)
   (add-hook (intern (concat (symbol-name x) "-mode-hook")) 'rainbow-delimiters-mode))
+
+(defun toggle-clj-keyword-string ()
+  (defun char-at-point ()
+    (interactive)
+    (buffer-substring-no-properties (point) (+ 1 (point))))
+
+  (defun clj-string-name (s)
+    (substring s 1 -1))
+
+  (defun clj-keyword-name (s)
+    (substring s 1))
+
+  (defun delete-and-extract-sexp ()
+    (let* ((begin (point)))
+      (forward-sexp)
+      (let* ((result (buffer-substring-no-properties begin (point))))
+        (delete-region begin (point))
+        result)))
+
+  (interactive)
+  (save-excursion
+    (if (equal 1 (point))
+        (message "beginning of file reached, this was probably a mistake.")
+      (cond ((equal "\"" (char-at-point))
+             (insert ":" (clj-string-name (delete-and-extract-sexp))))
+            ((equal ":" (char-at-point))
+             (insert "\"" (clj-keyword-name (delete-and-extract-sexp)) "\""))
+            (t (progn
+                 (backward-char)
+                 (toggle-clj-keyword-string)))))))
+
+(define-key clojure-mode-map (kbd "C-:") 'toggle-clj-keyword-string)
