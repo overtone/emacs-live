0fd6c4f72259a21c0be00d4a1b6cf589dd78f73b
Andrew Jones <me@andrewhjon.es>
2013-06-06 21:26:03 +0300

    Switch to Ruby mode when opening .rake, Gemfile or .gemspec files

--- packs/live/lang-pack/config/ruby-conf.el
+++ packs/live/lang-pack/config/ruby-conf.el
@@ -5,6 +5,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
 (defun ruby-interpolate ()
   "In a double quoted string, interpolate."
