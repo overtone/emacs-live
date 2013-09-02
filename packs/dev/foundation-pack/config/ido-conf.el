c71766e7fa1b5764bfc1d5f23b100ace17213036
Sam Aaron <samaaron@gmail.com>
2013-09-02 22:27:43 +0100

    Prefer flx library over flex for ido completions. Fixes #117.

--- packs/dev/foundation-pack/config/ido-conf.el
+++ packs/dev/foundation-pack/config/ido-conf.el
@@ -1,11 +1,15 @@
 (require 'idomenu)
 
+(live-add-pack-lib "flx")
+(require 'flx-ido)
+
 (ido-mode t)
+(flx-ido-mode 1)
 (setq ido-enable-prefix nil
-      ido-enable-flex-matching t
       ido-create-new-buffer 'always
       ido-max-prospects 10
-      ido-default-file-method 'selected-window)
+      ido-default-file-method 'selected-window
+      ido-everywhere 1)
 
 (icomplete-mode 1)
 
