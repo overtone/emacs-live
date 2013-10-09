2bb5e5d76b9c50828335e29416c3db94e697021d
solussd <joe@uwcreations.com>
2013-10-09 17:07:33 -0500

    Update built-in.el
    Preserve whitespace in markdown mode

--- packs/dev/foundation-pack/config/built-in.el
+++ packs/dev/foundation-pack/config/built-in.el
@@ -70,9 +70,11 @@
 
 ;;remove all trailing whitespace and trailing blank lines before
 ;;saving the file
+(defvar live-ignore-whitespace-modes '(markdown-mode))
 (defun live-cleanup-whitespace ()
-  (let ((whitespace-style '(trailing empty)) )
-    (whitespace-cleanup)))
+  (if (not (member major-mode live-ignore-whitespace-modes))
+      (let ((whitespace-style '(trailing empty)) )
+        (whitespace-cleanup))))
 
 (add-hook 'before-save-hook 'live-cleanup-whitespace)
 
