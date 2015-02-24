49e275234106a165a094677b0ab360e7f20c36ad
Bill Piel <bill@billpiel.com>
2015-02-23 21:29:35 -0500

    init.el: load fzy-locate.el; bind C-x SPC

--- init.el
+++ init.el
@@ -237,10 +237,10 @@
 
 (global-set-key (kbd  "C-,") 'beginning-of-line-text)
 
-(load-file "/home/bill/.emacs.d/wip/globalff.el")
+(load-file "/home/bill/.emacs.d/wip/fzy-locate.el")
 ;(setq globalff-databases "/home/bill/repos/emacs-live/locatedbs/vm.locatedb:/home/bill/repos/emacs-live/locatedbs/repos.locatedb")
 (setq globalff-databases "/home/bill/repos/emacs-live/locatedbs/repos.locatedb")
-(global-set-key (kbd "C-x SPC") 'globalff)
+(global-set-key (kbd "C-x SPC") 'fzy-locate)
 (global-set-key (kbd  "C-x p") 'ace-jump-mode-pop-mark)
 (global-set-key (kbd  "C-x x") 'rgrep)
 
