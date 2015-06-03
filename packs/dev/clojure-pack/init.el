c4252b97dc8fcd76fbd6a17ff94c9df11e19f1d3
Sam Aaron <samaaron@gmail.com>
2015-06-03 20:52:28 +0100

    Update libs

--- packs/dev/clojure-pack/init.el
+++ packs/dev/clojure-pack/init.el
@@ -6,9 +6,13 @@
 (live-add-pack-lib "uuid")
 (require 'uuid)
 
+(live-add-pack-lib "edn")
+(require 'edn)
+
 (live-load-config-file "paredit-conf.el")
 (live-load-config-file "mic-paren-conf.el")
 (live-load-config-file "auto-complete-conf.el")
+
 (live-load-config-file "clojure-conf.el")
 (live-load-config-file "cider-conf.el")
 (live-load-config-file "highlight-flash-conf.el")
