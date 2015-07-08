06253cf804027578c33524c218e9ce8e88e0f8d1
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-07-08 21:07:30 +0200

    Add squiggly-clojure (aka flycheck-clojure) linting.
    Uses flycheck-pos-tip for popups as well. Follows https://github.com/clojure-emacs/example-config

--- packs/dev/clojure-pack/init.el
+++ packs/dev/clojure-pack/init.el
@@ -12,10 +12,9 @@
 (live-load-config-file "paredit-conf.el")
 (live-load-config-file "mic-paren-conf.el")
 (live-load-config-file "auto-complete-conf.el")
-
 (live-load-config-file "clojure-conf.el")
 (live-load-config-file "cider-conf.el")
 (live-load-config-file "highlight-flash-conf.el")
-
+(live-load-config-file "flycheck-clojure-conf.el")
 
 ;;(live-load-config-file "overtone-conf.el") TODO - fix for cider
