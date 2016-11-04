d51d509407905c36428d9c00a035faeb9c6fe606
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-11-04 16:24:02 -0700

    Fix linum area when text is increased (EmacsWiki)

--- lib/live-core.el
+++ lib/live-core.el
@@ -273,3 +273,15 @@ children of DIRECTORY."
     (with-current-buffer buf
       (write-file fname)
       (auto-save-mode 1))))
+
+;; From https://www.emacswiki.org/emacs/LineNumbers#toc14
+(defun live-linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+                      (ceiling (* (if (boundp 'text-scale-mode-step)
+                                      (expt text-scale-mode-step
+                                            text-scale-mode-amount) 1)
+                                  (if (car (window-margins))
+                                      (car (window-margins)) 1)
+                                  ))))
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
