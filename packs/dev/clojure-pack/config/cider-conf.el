bf9a8dd887ef5d4d0bc47f8405ab0115add126a6
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-04-21 13:15:03 -0700

    Delete deprecated clj-refactor bindings (now in clojure-mode)

--- packs/dev/clojure-pack/config/cider-conf.el
+++ packs/dev/clojure-pack/config/cider-conf.el
@@ -58,6 +58,3 @@
 (add-hook 'clojure-mode-hook (lambda ()
                                (clj-refactor-mode 1)
                                (cljr-add-keybindings-with-prefix "C-c C-m")))
-
-(define-key clojure-mode-map (kbd "C-:") 'cljr-cycle-stringlike)
-(define-key clojure-mode-map (kbd "C->") 'cljr-cycle-coll)
