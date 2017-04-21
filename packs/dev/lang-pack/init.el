895542b59ef6e4c676beb084e61975cb2c7aeedd
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-04-21 13:06:43 -0700

    Get rid of ruby-mode

--- packs/dev/lang-pack/init.el
+++ packs/dev/lang-pack/init.el
@@ -6,7 +6,6 @@
 (live-load-config-file "yaml-conf.el")
 (live-load-config-file "ioke-conf.el")
 (live-load-config-file "auto-modes.el")
-(live-load-config-file "ruby-conf.el")
 (live-load-config-file "markdown-conf.el")
 (live-load-config-file "supercollider-conf.el")
 (live-load-config-file "js2-conf.el")
