48b0291eda8b1a8649634e1ac485686f906088a2
Joseph Fosco <joseph@josephfosco.com>
2021-12-24 16:10:46 -0600

    changes made on new computer

---
+++ elpa/seq-2.22/seq-autoloads.el
@@ -0,0 +1,33 @@
+;;; seq-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "seq-24" "seq-24.el" (0 0 0 0))
+;;; Generated autoloads from seq-24.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "seq-24" '("seq")))
+
+;;;***
+
+;;;### (autoloads nil "seq-25" "seq-25.el" (0 0 0 0))
+;;; Generated autoloads from seq-25.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "seq-25" '("seq--when-emacs-25-p")))
+
+;;;***
+
+;;;### (autoloads nil nil ("seq-pkg.el" "seq.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; seq-autoloads.el ends here
